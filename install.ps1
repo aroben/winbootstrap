9b750d5bc9ff163dc927b48b8997e15fcc4be2d2
Adam Roben <adam@roben.org>
2012-07-11 12:37:35 -0400

    Silence reg output

--- install.ps1
+++ install.ps1
@@ -29,7 +29,7 @@ function Install-StartupScripts {
     Copy-Item $script.Path C:\cygwin\home\Administrator
   }
 
-#reg import (Join-Path $scriptDirectory bootstrap-startup.reg)
+  reg import (Join-Path $scriptDirectory bootstrap-startup.reg) | Out-Null
 }
 
 Install-Cygwin
