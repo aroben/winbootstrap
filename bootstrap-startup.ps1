de14bb48106225295f04a214e913092da58bb753
Adam Roben <adam@roben.org>
2012-07-11 12:35:25 -0400

    Simplify regenerating SSH keys

--- bootstrap-startup.ps1
+++ bootstrap-startup.ps1
@@ -3,8 +3,7 @@ $ErrorActionPreference = "Stop"
 
 function Start-Sshd {
   # First we generate new keys in case we're launching on a new VM.
-  Remove-Item C:\cygwin\etc\ssh_host*_key*
-  C:\cygwin\bin\bash.exe --login -- /usr/bin/ssh-host-config --no
+  C:\cygwin\bin\bash.exe --login -- /usr/bin/ssh-host-config --yes
 
   Set-Service sshd -StartupType Automatic
   net start sshd
