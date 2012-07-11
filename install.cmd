45c97fb77f8028a76ab950d3547cb4df883f046b
Adam Roben <adam@roben.org>
2012-07-11 09:03:08 -0400

    Get rid of security warning when running install.cmd

--- install.cmd
+++ install.cmd
@@ -1,1 +1,1 @@
-powershell -ExecutionPolicy Unrestricted -File %~dp0\install.ps1 > C:\Users\Administrator\winbootstrap.log
+powershell -ExecutionPolicy Bypass -File %~dp0\install.ps1 > C:\Users\Administrator\winbootstrap.log
